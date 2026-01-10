081bdfe6b3dce27ca693fe684d824bf2c918a051
PriyanshMaheswari777 <PriyanshMaheswari777@gmail.com>
2026-01-10 17:37:30 +0530

    Uploading backend

--- backend/main.py
+++ backend/main.py
@@ -25,7 +25,7 @@ async def startup_event():
 # CORS middleware to allow frontend requests
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000", "http://127.0.0.1:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
